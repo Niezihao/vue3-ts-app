5ec8b7b4cce9000bac6f54bd8fe363ce53650562
niezihao <1332421989@qq.com>
2023-07-17 10:22:47 +0800

    fix: router挂载顺序问题修复

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@
  * @Author: niezihao 1332421989@qq.com
  * @Date: 2023-07-14 15:45:09
  * @LastEditors: niezihao 1332421989@qq.com
- * @LastEditTime: 2023-07-14 17:55:13
+ * @LastEditTime: 2023-07-17 09:58:22
  * @FilePath: \vue3-ts-app\src\main.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -10,9 +10,9 @@ import { createApp } from "vue";
 import "./style.css";
 import store from "@/store/index";
 import App from "@/App.vue";
-import router from '@/router/index'
+import router from "@/router/index";
 
 const app = createApp(App);
-app.use(store).mount("#app");
-app.use(router)
-
+app.use(router);
+app.use(store);
+app.mount("#app");
